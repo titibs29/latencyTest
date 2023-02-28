8bf05fa2099430a6d43cabe0f999a88c6a046495
tri <thibaut.rizzoli@spacebel.be>
2023-02-28 14:42:38 +0100

    no-warnings-update

--- multithread/pluriping.cpp
+++ multithread/pluriping.cpp
@@ -6,32 +6,32 @@
 #include <cstring>
 #include <iomanip>
 
-void ping(int tid, int rounds, long long int sleep_time, std::mutex &ping_mutex, std::queue <std::chrono::time_point<std::chrono::high_resolution_clock>> &ping_queue, std::queue <int> &ping_id_queue);
+void ping(unsigned int tid, unsigned int rounds, long long unsigned int sleep_time, std::mutex &ping_mutex, std::queue <std::chrono::time_point<std::chrono::high_resolution_clock>> &ping_queue, std::queue <unsigned int> &ping_id_queue);
 
 
 int main(int argc, char *argv[])
 {
 
     //define variables
-    int id = 0;
-    long long int diff = 0;
+    unsigned int id = 0, i = 0, j = 0;
+    long long unsigned int diff = 0;
     std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
 
     
-    int rounds = 5;
-    long long int sleep_time = 1000000000; // 1 second
-    int nb_threads = 2;
+    long long unsigned int rounds = 5;
+    long long unsigned int sleep_time = 1000000000; // 1 second
+    long long unsigned int nb_threads = 2;
 
 
 
     // use the -t option to set the wait time, -r to set the number of ping, -n to set the number of threads
     if(argc > 1)
     {
-        for(int i = 1; i < argc; i++)
+        for(i = 1; i < static_cast<unsigned int>(argc); i++)
         {
             if(strcmp(argv[i], "-t") == 0)
             {
-                sleep_time = std::stoll(argv[i+1]);
+                sleep_time = std::stoull(argv[i+1]);
                 if (sleep_time < 1)
                 {
                     std::cout << "Error: the wait time must be greater than 0\n";
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
             }
             else if(strcmp(argv[i], "-r") == 0)
             {
-                rounds = std::stoi(argv[i+1]);
+                rounds = std::stoull(argv[i+1]);
                 if (rounds < 1)
                 {
                     std::cout << "Error: the number of rounds must be greater than 0\n";
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
             }
             else if(strcmp(argv[i], "-n") == 0)
             {
-                nb_threads = std::stoi(argv[i+1]);
+                nb_threads = std::stoull(argv[i+1]);
                 if (nb_threads < 1)
                 {
                     std::cout << "Error: the number of threads must be greater than 0\n";
@@ -62,6 +62,7 @@ int main(int argc, char *argv[])
                 std::cout << "Default values: wait_time = 1s, rounds = 5, nb_threads = 2\n";
                 return 0;
             }
+            
         }
     }
 
@@ -69,7 +70,7 @@ int main(int argc, char *argv[])
     std::queue <std::chrono::time_point<std::chrono::high_resolution_clock>> ping_queue;
 
     //create the ping id queue
-    std::queue <int> ping_id_queue;
+    std::queue <unsigned int> ping_id_queue;
 
     // mutex to protect the ping variable
     std::mutex ping_mutex;
@@ -78,30 +79,30 @@ int main(int argc, char *argv[])
     std::thread *threads = new std::thread[nb_threads];
 
     // create queue to stock the ping times
-    std::queue <long long int> *diff_queues = new std::queue <long long int> [nb_threads];
+    std::queue <long long unsigned int> *diff_queues = new std::queue <long long unsigned int> [nb_threads];
 
     // create a array to stock last and actual ping times
     std::chrono::time_point<std::chrono::high_resolution_clock> *actual = new std::chrono::time_point<std::chrono::high_resolution_clock> [nb_threads];
     std::chrono::time_point<std::chrono::high_resolution_clock> *last = new std::chrono::time_point<std::chrono::high_resolution_clock> [nb_threads];
 
     // fill the array with the default value
-    for(int i = 0; i < nb_threads; i++)
+    for(i = 0; i < nb_threads; i++)
     {
         actual[i] = std::chrono::time_point<std::chrono::high_resolution_clock>();
         last[i] = std::chrono::time_point<std::chrono::high_resolution_clock>();
     }
 
     // create the min, max, average and median deviation arrays
-    long long int *min = new long long int[nb_threads];
-    long long int *max = new long long int[nb_threads];
-    long long int *avg = new long long int[nb_threads];
+    long long unsigned int *min = new long long unsigned int[nb_threads];
+    long long unsigned int *max = new long long unsigned int[nb_threads];
+    long long unsigned int *avg = new long long unsigned int[nb_threads];
     double *percent = new double[nb_threads];
 
     
 
     // start the threads
 
-    for(int i = 0; i < nb_threads; i++)
+    for(i = 0; i < nb_threads; i++)
     {
         threads[i] = std::thread(ping, i, rounds, sleep_time, std::ref(ping_mutex), std::ref(ping_queue), std::ref(ping_id_queue));
     }
@@ -112,7 +113,7 @@ int main(int argc, char *argv[])
     std::cout << "--- Ping times ---\n";
 
     // get the pings and stock it in the pings array based on the ping id queue
-    for(int i = 0; i < nb_threads * (rounds + 1); i++)
+    for(i = 0; i < nb_threads * (rounds + 1); i++)
     {
         //wait for a ping
         while(ping_queue.empty() && ping_id_queue.empty())
@@ -130,7 +131,7 @@ int main(int argc, char *argv[])
         // print the ping time
         if(last[id] != std::chrono::time_point<std::chrono::high_resolution_clock>() && actual[id] != std::chrono::time_point<std::chrono::high_resolution_clock>())
         {
-            diff = std::chrono::duration_cast<std::chrono::nanoseconds>(actual[id] - last[id]).count();
+            diff = static_cast<unsigned>(std::chrono::duration_cast<std::chrono::nanoseconds>(actual[id] - last[id]).count());
             diff -= sleep_time;
             std::cout << "Ping " << id << " latency : " << diff << " ns\n";
 
@@ -145,13 +146,13 @@ int main(int argc, char *argv[])
     }
 
     //for each thread
-    for(int i = 0; i < nb_threads; i++)
+    for(i = 0; i < nb_threads; i++)
     {
         // calculate the min, max and average ping time
         min[i] = diff_queues[i].front();
         max[i] = 0;
         avg[i] = 0;
-        for(int j = 0; j < rounds; j++)
+        for(j = 0; j < rounds; j++)
         {
             // get the ping time
             diff = diff_queues[i].front();
@@ -173,7 +174,7 @@ int main(int argc, char *argv[])
         avg[i] /= rounds;
 
         // calculate max in percent
-        percent[i] = ((double)max[i] * 100.0) / (double)sleep_time;
+        percent[i] = (static_cast<double>(max[i]) * 100.0) / static_cast<double>(sleep_time);
 
         // print the min, max and average ping time
         std::cout << "\n--- Thread " << i << " statistics ---\n";
@@ -184,7 +185,7 @@ int main(int argc, char *argv[])
         std::cout << "Max in percent : " << percent[i] << "%\n";     
     }
     // join the threads
-    for(int i = 0; i < nb_threads; i++)
+    for(i = 0; i < nb_threads; i++)
     {
         threads[i].join();
     }
@@ -202,9 +203,10 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void ping(int tid, int rounds, long long int sleep_time, std::mutex &ping_mutex, std::queue <std::chrono::time_point<std::chrono::high_resolution_clock>> &ping_queue, std::queue <int> &ping_id_queue)
+void ping(unsigned int tid, unsigned int rounds, long long unsigned int sleep_time, std::mutex &ping_mutex, std::queue <std::chrono::time_point<std::chrono::high_resolution_clock>> &ping_queue, std::queue <unsigned int> &ping_id_queue)
 {
     std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    unsigned int i =0;
     // get the start time
     start = std::chrono::high_resolution_clock::now();
 
@@ -215,18 +217,16 @@ void ping(int tid, int rounds, long long int sleep_time, std::mutex &ping_mutex,
     ping_mutex.unlock();
 
     // loop until the number of ping is reached
-    for (int i = 0; i < rounds; i++)
+    for (i = 0; i < rounds+1; i++)
     {
         // get the start time
         start = std::chrono::high_resolution_clock::now();
 
-        // wait for the ping to be done
-        end = std::chrono::high_resolution_clock::now();
+        do {
 
-        while (std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() < sleep_time)
-        {
             end = std::chrono::high_resolution_clock::now();
-        }
+
+        } while (static_cast<long long unsigned int>(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()) < sleep_time);
 
         // add the ping to the ping time array
         ping_mutex.lock();
