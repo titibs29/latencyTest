0c712f11889abdb1f0b8bedc741f4267aa44f9d4
titibs29 <thibaut.rizzoli@hotmail.be>
2023-03-01 10:27:13 +0100

    debug

--- src/sleepTest.cpp
+++ src/sleepTest.cpp
@@ -15,19 +15,19 @@
 int main(int argc, char *argv[])
 {
     std::chrono::time_point<std::chrono::system_clock> start, end;
-    long long diff;
-    int i, rounds = 5;
-    long long sum = 0;
-    long long sleepTime = 1000000000L;            // sleep time in nanosecond
+    long long unsigned int diff;
+    long long unsigned int i, rounds = 5;
+    long long unsigned int sum = 0;
+    long long unsigned int sleepTime = 1000000000L;            // sleep time in nanosecond
 
     // use the -t option to change the sleep time or use the -r option to change the number of rounds
     if (argc > 1)
     {
-        for (i = 1; i < argc; i++)
+        for (i = 1; i < static_cast<long long unsigned int>(argc); i++)
         {
             if (strcmp(argv[i], "-t") == 0)
             {
-                long long temp = atoll(argv[i+1]);
+                long long unsigned int temp = strtoull(argv[i+1], NULL, 10);
                 if (temp > 0)
                 {
                     sleepTime = temp;
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
             }
             else if (strcmp(argv[i], "-r") == 0)
             {
-                rounds = atoi(argv[i+1]);
+                rounds = strtoull(argv[i+1], NULL, 10);
             }
             else if (strcmp(argv[i], "-h") == 0){
                 printf("usage : %s [-t sleepTime] [-r rounds] \n", argv[0]);
@@ -44,9 +44,9 @@ int main(int argc, char *argv[])
         }
     }
 
-    long long *diffs = new long long[rounds];
+    long long unsigned int *diffs = new long long unsigned int[rounds];
 
-    printf("latency test for %lld ns\n", sleepTime);
+    printf("latency test for %lld ns over %lld rounds\n", sleepTime, rounds);
 
     for (i = 0; i < rounds; i++)
     {   
@@ -81,11 +81,11 @@ int main(int argc, char *argv[])
     {
         sum += diffs[i];
     }
-    long long avg = sum / rounds;
+    long long unsigned int avg = sum / rounds;
 
     // calculate ther min and max
-    long long min = diffs[0];
-    long long max = diffs[0];
+    long long unsigned int min = diffs[0];
+    long long unsigned int max = diffs[0];
     for (i = 0; i < rounds; i++)
     {
         if (diffs[i] < min)
@@ -99,12 +99,12 @@ int main(int argc, char *argv[])
     }
 
     // calculate the standard deviation
-    long long sumOfSquares = 0;
+    long long unsigned int sumOfSquares = 0;
     for (i = 0; i < rounds; i++)
     {
         sumOfSquares += (diffs[i] - avg) * (diffs[i] - avg);
     }
-    double stdDev = sqrt(static_cast<double>(sumOfSquares / rounds));
+    double stdDev = sqrt(static_cast<double>(sumOfSquares) / static_cast<double>(rounds));
 
     // calculates the percentage of the average on the sleep time
     double percentage = (static_cast<double>(avg) * 100.0) / static_cast<double>(sleepTime);
@@ -112,7 +112,7 @@ int main(int argc, char *argv[])
 
     // print the results
     printf(" --- latency results --- \n");
-    printf(" %d rounds completed\n", rounds);
+    printf(" %lld rounds completed\n", rounds);
     printf("latency min / max / avg / mdev (nanosecond) : %lld / %lld / %lld / %.0f \n", min, max, avg, stdDev);
     printf("avg percentage over sleep time : %.3f %% \n", percentage);
 
